9734eea26e3dd6c2f918e747ed5ccc4b517155d6
David <lyondyasnty@gmail.com>
2024-08-13 18:11:50 +0300

    changes

---
+++ clipboard/simple-tailwind-starter-main/clipboard/simple-tailwind-starter-main/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+
+    extend: {
+      colors: {
+        StrongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210 ,10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      FontFamily: {
+        sans: ['Bai Jamjuree','San-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
